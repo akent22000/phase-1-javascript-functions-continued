c6f21e6602745ae0a55b2ca3bf9f94170e71ef6d
amandalocher <akent22000@gmail.com>
2024-01-30 18:28:19 -0500

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // code your solution here
+function saturdayFun(activity = 'roller-skate') {
+    return(`This Saturday, I want to ${activity}!`)
+
+}
+
+saturdayFun()
+saturdayFun('bath my dog')
+
+
+function mondayWork(activity = 'go to the office'){
+    return(`This Monday, I will ${activity}.`)
+
+}
+
+mondayWork()
+mondayWork('work from home.')
+
+
+
+function wrapAdjective(wrapper = "*") {
+    function emphatic(string = "specialr") {
+        return `You are ${wrapper}${string}${wrapper}!`
+    }
+    
+    return emphatic
+    
+}
+
+let result = wrapAdjective("||")
+result("a dedicated programmer")
